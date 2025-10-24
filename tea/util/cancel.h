df1ad9cf3743eabcb7ec96144ca5758e63626d77
gmusya <ggmusya@gmail.com>
2025-10-24 15:18:38 +0000

    X-673 Use CancelToken in samovar backoff

--- tea/util/cancel.h
+++ tea/util/cancel.h
@@ -1,17 +1,39 @@
 #pragma once
 
-#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
 
 namespace tea {
 
 class CancelToken {
  public:
-  void Cancel() { state_.store(true); }
+  void Cancel() {
+    std::lock_guard lg(lock_);
+    state_ = State::kCancelled;
+    cancel_condition_.notify_all();
+  }
 
-  bool IsCancelled() const { return state_.load(); }
+  bool IsCancelled() const {
+    std::lock_guard lg(lock_);
+    return state_ == State::kCancelled;
+  }
+
+  template <typename Rep, typename Period>
+  bool WaitFor(std::chrono::duration<Rep, Period> duration) const {
+    std::unique_lock lg(lock_);
+
+    cancel_condition_.wait_for(lg, duration, [&]() { return state_ == State::kCancelled; });
+
+    return state_ == State::kCancelled;
+  }
 
  private:
-  std::atomic<bool> state_{};
+  enum class State { kRunning, kCancelled };
+
+  mutable std::mutex lock_;
+  mutable std::condition_variable cancel_condition_;
+  State state_{State::kRunning};
 };
 
 }  // namespace tea
