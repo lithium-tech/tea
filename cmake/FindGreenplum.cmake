42bf20f1356661ab744992078ab9ce2848290ceb
Andrey Sokolov <sokolov.andrey.yurevich@gmail.com>
2026-08-27 09:33:19 +0300

    Change FindGreenplum.cmake to compile TEA from gpcontrib

--- cmake/FindGreenplum.cmake
+++ cmake/FindGreenplum.cmake
@@ -13,13 +13,6 @@ else()
 endif()
 
 execute_process(
-        COMMAND ${PG_CONFIG} "--includedir-server"
-        OUTPUT_VARIABLE gp_include_server
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-set(Greenplum_INCLUDE_SERVER "${gp_include_server}" CACHE PATH "Greenplum include-server.")
-
-execute_process(
         COMMAND ${PG_CONFIG} "--ldflags"
         OUTPUT_VARIABLE gp_ldflags
         OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -55,30 +48,7 @@ execute_process(
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 set(Greenplum_LIBDIR "${gp_libdir}" CACHE PATH "Greenplum libdir.")
-find_library(LIBPQ pq PATHS "${Greenplum_LIBDIR}" REQUIRED NO_DEFAULT_PATH)
-add_library(pq SHARED IMPORTED)
-set_property(TARGET pq PROPERTY IMPORTED_LOCATION "${LIBPQ}")
 
-find_library(LIBGPLDAP ldap PATHS "${Greenplum_LIBDIR}" NO_DEFAULT_PATH)
-if(LIBGPLDAP)
-        target_link_libraries(pq INTERFACE ${LIBGPLDAP})
-endif()
-find_library(LIBGPLDAP_R ldap_r PATHS "${Greenplum_LIBDIR}" NO_DEFAULT_PATH)
-if(LIBGPLDAP_R)
-        target_link_libraries(pq INTERFACE ${LIBGPLDAP_R})
-endif()
-find_library(LIBGPLBER lber PATHS "${Greenplum_LIBDIR}" NO_DEFAULT_PATH)
-if(LIBGPLBER)
-        target_link_libraries(pq INTERFACE ${LIBGPLBER})
-endif()
-
-execute_process(
-        COMMAND ${PG_CONFIG} "--includedir"
-        OUTPUT_VARIABLE gp_includedir
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-set(Greenplum_INCLUDE "${gp_includedir}" CACHE PATH "Greenplum includedir.")
-target_include_directories(pq INTERFACE "${Greenplum_INCLUDE}")
 
 execute_process(
         COMMAND ${PG_CONFIG} "--version"
