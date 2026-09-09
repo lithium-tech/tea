80b5d8ab8df90bb08ab192a74418f689ef30084d
German Perov <ggmusya@gmail.com>
2026-09-09 17:52:01 +0300

    impl

--- tea/table/ut/profile_to_tables_test.cpp
+++ tea/table/ut/profile_to_tables_test.cpp
@@ -111,4 +111,106 @@ TEST(ProfileToTables, OneTableMultipleProfiles) {
   EXPECT_EQ(result, expected);
 }
 
+TEST(UsernameToProfile, Trivial) {
+  const std::string_view kTestJsonConfig = R"__({
+    "profile-to-username": {
+        "someprofile": ["name1"]
+    }
+})__";
+
+  ASSIGN_OR_FAIL(auto result, GetUsernameToProfileMapping(std::string(kTestJsonConfig)));
+
+  std::unordered_map<std::string, std::string> expected = {{"name1", "someprofile"}};
+  EXPECT_EQ(result, expected);
+}
+
+TEST(UsernameToProfile, Empty) {
+  const std::string_view kTestJsonConfig = R"__({
+    "profile-to-username": {}
+})__";
+
+  ASSIGN_OR_FAIL(auto result, GetUsernameToProfileMapping(std::string(kTestJsonConfig)));
+
+  std::unordered_map<std::string, std::string> expected;
+  EXPECT_EQ(result, expected);
+}
+
+TEST(UsernameToProfile, MissingRootFieldIsNotAnError) {
+  const std::string_view kTestJsonConfig = R"__({
+    "a": "b"
+})__";
+
+  ASSIGN_OR_FAIL(auto result, GetUsernameToProfileMapping(std::string(kTestJsonConfig)));
+
+  std::unordered_map<std::string, std::string> expected;
+  EXPECT_EQ(result, expected);
+}
+
+TEST(UsernameToProfile, IncorrectJson) {
+  const std::string_view kTestJsonConfig = R"__({
+   qwe{{}{}{}{}ad}
+})__";
+
+  auto maybe_result = GetUsernameToProfileMapping(std::string(kTestJsonConfig));
+  ASSERT_NE(maybe_result.status(), arrow::Status::OK());
+
+  EXPECT_EQ(maybe_result.status().message(), "Profile-to-username parsing error: not a valid JSON");
+}
+
+TEST(UsernameToProfile, RootIsNotAnObject) {
+  const std::string_view kTestJsonConfig = R"__(
+   "q"
+)__";
+
+  auto maybe_result = GetUsernameToProfileMapping(std::string(kTestJsonConfig));
+  ASSERT_NE(maybe_result.status(), arrow::Status::OK());
+
+  EXPECT_EQ(maybe_result.status().message(), "Profile-to-username parsing error: root is not an object");
+}
+
+TEST(UsernameToProfile, ValueIsNotAnArray) {
+  const std::string_view kTestJsonConfig = R"__({
+    "profile-to-username": {
+      "someprofile": "name1,name2"
+    }
+})__";
+
+  auto maybe_result = GetUsernameToProfileMapping(std::string(kTestJsonConfig));
+  ASSERT_NE(maybe_result.status(), arrow::Status::OK());
+
+  EXPECT_EQ(maybe_result.status().message(),
+            "Profile-to-username parsing error: value for profile 'someprofile' is not an array");
+}
+
+TEST(UsernameToProfile, ElementIsNotAString) {
+  const std::string_view kTestJsonConfig = R"__({
+    "profile-to-username": {
+      "someprofile": ["name1", "name2", 123]
+    }
+})__";
+
+  auto maybe_result = GetUsernameToProfileMapping(std::string(kTestJsonConfig));
+  ASSERT_NE(maybe_result.status(), arrow::Status::OK());
+
+  EXPECT_EQ(maybe_result.status().message(),
+            "Profile-to-username parsing error: element for key 'someprofile' is not a string");
+}
+
+TEST(UsernameToProfile, UsernameClaimedByMultipleProfiles) {
+  const std::string_view kTestJsonConfig = R"__({
+    "profile-to-username": {
+      "someprofile1": ["name1", "name3", "name5", "name7", "shared1"],
+      "someprofile2": ["name1", "name2", "name5", "name6", "shared2"],
+      "someprofile3": ["name1", "name2", "name3", "name4", "shared3"]
+    }
+})__";
+
+  ASSIGN_OR_FAIL(auto result, GetUsernameToProfileMapping(std::string(kTestJsonConfig)));
+
+  std::unordered_map<std::string, std::string> expected = {{"name7", "someprofile1"},  {"name4", "someprofile3"},
+                                                            {"name6", "someprofile2"},  {"shared1", "someprofile1"},
+                                                            {"shared2", "someprofile2"}, {"shared3", "someprofile3"}};
+  EXPECT_EQ(result, expected);
+}
+
 }  // namespace tea
